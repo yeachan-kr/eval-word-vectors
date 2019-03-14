7fe1c74adcbab8204187a31e66196eabdee8117a
yeachan-kr <yeachan.kr@gmail.com>
2019-03-14 13:38:31 +0900

    minor

--- all_wordsim.py
+++ all_wordsim.py
@@ -7,11 +7,23 @@ from ranking import *
 if __name__=='__main__':  
   word_vec_file = sys.argv[1]
   word_sim_dir = sys.argv[2]
+  dimension = int(sys.argv[3])
+  oov = int(sys.argv[3])
   
-  word_vecs = read_word_vectors(word_vec_file)
-  print '================================================================================='
-  print "%6s" %"Serial", "%20s" % "Dataset", "%15s" % "Num Pairs", "%15s" % "Not found", "%15s" % "Rho"
-  print '================================================================================='
+  print(sys.argv)
+  
+  v = []
+  for i, filename in enumerate(os.listdir(word_sim_dir)):
+      for line in open(os.path.join(word_sim_dir, filename),'r'):
+          line = line.strip().lower()
+          word1, word2, val = line.split()
+          v.append(word1)
+          v.append(word2)
+  
+  word_vecs = read_word_vectors(word_vec_file, v, dimension, oov)
+  print('=================================================================================')
+  print("%6s" %"Serial", "%20s" % "Dataset", "%15s" % "Num Pairs", "%15s" % "Not found", "%15s" % "Rho")
+  print('=================================================================================')
 
   for i, filename in enumerate(os.listdir(word_sim_dir)):
     manual_dict, auto_dict = ({}, {})
@@ -25,6 +37,6 @@ if __name__=='__main__':
       else:
         not_found += 1
       total_size += 1    
-    print "%6s" % str(i+1), "%20s" % filename, "%15s" % str(total_size),
-    print "%15s" % str(not_found),
-    print "%15.4f" % spearmans_rho(assign_ranks(manual_dict), assign_ranks(auto_dict))
+    print("%6s" % str(i+1), "%20s" % filename, "%15s" % str(total_size), end=' ')
+    print("%15s" % str(not_found), end=' ')
+    print("%15.4f" % spearmans_rho(assign_ranks(manual_dict), assign_ranks(auto_dict)), end='\n')
